7af590247cb5ccf9943ee5519238bee146c1c238
ngeran <ngeran@gmail.com>
2025-03-08 17:43:23 -0500

    lazygit

--- home-manager/features/cli/nixvim/plugins/lazygit.nix
+++ home-manager/features/cli/nixvim/plugins/lazygit.nix
@@ -1,8 +1,14 @@
 {
-  programs.nixvim = {
-    plugins.lazygit = {
-      enable = true;
-     
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui.showIcons = true;
+      gui.theme = {
+        lightTheme = false;
+        activeBorderColor = [ "green" "bold" ];
+        inactiveBorderColor = [ "grey" ];
+        selectedLineBgColor = [ "blue" ];
+      };
     };
   };
 }
