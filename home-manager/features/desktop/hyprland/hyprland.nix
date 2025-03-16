fbad80dc305a0e2f93abae38337e0ec867a1f130
ngeran <ngeran@gmail.com>
2025-03-16 10:24:34 -0400

    modify the hyprland config

--- home-manager/features/desktop/hyprland/hyprland.nix
+++ home-manager/features/desktop/hyprland/hyprland.nix
@@ -120,7 +120,7 @@
         "move 990 60,size 900 170,pin,noinitialfocus,class:(showmethekey-gtk)"
         "noborder,nofocus,class:(showmethekey-gtk)"
 
-        "workspace 3,class:(obsidian)"
+        "workspace 9,class:(obsidian)"
         "workspace 3,class:(zathura)"
         "workspace 4,class:(com.obsproject.Studio)"
         "workspace 5,class:(telegram)"
