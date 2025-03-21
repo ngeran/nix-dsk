c86f7017c3b6e739c702440815e8e5566f5fc3a2
ngeran <ngeran@gmail.com>
2025-03-21 15:22:25 -0400

    nixvim plugins

--- home-manager/features/cli/nixvim/plugins/indent-blankline.nix
+++ home-manager/features/cli/nixvim/plugins/indent-blankline.nix
@@ -1,35 +1,47 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
+
 {
-  plugins = {
-    indent-blankline = {
-      enable =
-        (!lib.hasAttr "indent" config.plugins.snacks.settings)
-        || !config.plugins.snacks.settings.indent.enabled;
+  programs.nixvim = {
+    enable = true;
+    plugins = {
+      indent-blankline = {
+        enable =
+          (!lib.hasAttr "indent" config.programs.nixvim.plugins.snacks.settings)
+          || !config.programs.nixvim.plugins.snacks.settings.indent.enabled;
 
-      lazyLoad.settings.event = "DeferredUIEnter";
+        lazyLoad.settings.event = "DeferredUIEnter";
 
-      settings = {
-        scope.enabled = false;
+        settings = {
+          scope.enabled = false;
+        };
+      };
+      snacks = {
+        enable = true;
+        settings = {
+          indent = {
+            enabled = true;
+          };
+        };
       };
     };
-  };
 
-  keymaps = lib.optionals config.plugins.indent-blankline.enable [
-    {
-      mode = "n";
-      key = "<leader>ui";
-      action = "<cmd>IBLToggle<CR>";
-      options = {
-        desc = "Indent-Blankline toggle";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>uI";
-      action = "<cmd>IBLToggleScope<CR>";
-      options = {
-        desc = "Indent-Blankline Scope toggle";
-      };
-    }
-  ];
+    keymaps = lib.optionals config.programs.nixvim.plugins.indent-blankline.enable [
+      {
+        mode = "n";
+        key = "<leader>ui";
+        action = "<cmd>IBLToggle<CR>";
+        options = {
+          desc = "Indent-Blankline toggle";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>uI";
+        action = "<cmd>IBLToggleScope<CR>";
+        options = {
+          desc = "Indent-Blankline Scope toggle";
+        };
+      }
+    ];
+  };
 }
