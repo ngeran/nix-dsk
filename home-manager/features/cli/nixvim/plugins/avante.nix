5b40f7874f48edab52d8fa0cd8e7db84cad6ab19
ngeran <ngeran@gmail.com>
2025-03-22 17:47:34 -0400

    avante

--- home-manager/features/cli/nixvim/plugins/avante.nix
+++ home-manager/features/cli/nixvim/plugins/avante.nix
@@ -1,43 +1,17 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
   programs.nixvim = {
+    # Enable the plugins section under programs.nixvim
     plugins = {
       # Configure avante plugin
       avante = {
         enable = true;
-        package = pkgs.vimPlugins.avante-nvim.overrideAttrs {
-          patches = [
-            # Optionally, patch Avante plugin
-            (pkgs.fetchpatch {
-              url = "https://github.com/doodleEsc/avante.nvim/commit/a5438d0f16208b7ae9e97ae354bed5ec16b4f9ed.patch";
-              hash = "sha256-KyfO9dE27yMXOQhpit7jmzkvnfM7b5kr2Acoh011lXA=";
-            })
-          ];
-        };
-        settings = {
-          mappings = {
-            files = {
-              add_current = "<leader>a.";  # Avante mapping
-            };
-          };
-        };
       };
 
       # Configure copilot-lua plugin, disable copilot-vim
       copilot-lua = {
         enable = true;  # Enable copilot-lua here
       };
-
-      # Disable copilot-vim if you are using copilot-lua
-      copilot-vim = {
-        enable = false;  # Disable copilot-vim here
-      };
-    };
+    };  # This closes the plugins block
 
     # Add keymaps for both plugins without which-key
     keymaps = lib.optionals config.plugins.avante.enable [
