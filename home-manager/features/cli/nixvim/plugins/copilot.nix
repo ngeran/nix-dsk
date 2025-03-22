4848dd27aef4ca2dbcd123341a32188912bba640
ngeran <ngeran@gmail.com>
2025-03-22 16:59:43 -0400

    copilot

--- home-manager/features/cli/nixvim/plugins/copilot.nix
+++ home-manager/features/cli/nixvim/plugins/copilot.nix
@@ -1,19 +1,7 @@
 {
-  programs.nixvim.plugins = {
-    copilot-vim = {
+  programs.nixvims = {
+    plugin.copilot-vim = {
       enable = true;
-      postBuild = ''
-        mkdir -p $out/share/nvim/runtime/doc
-        nvim -u NONE -i NONE -es +"helptags $out/share/nvim/runtime/doc" +q
-      '';
-    };
-
-    copilot-chat = {
-      enable = true;
-      postBuild = ''
-        mkdir -p $out/share/nvim/runtime/doc
-        nvim -u NONE -i NONE -es +"helptags $out/share/nvim/runtime/doc" +q
-      '';
     };
   };
 }
