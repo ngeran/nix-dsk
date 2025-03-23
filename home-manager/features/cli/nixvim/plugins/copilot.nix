da8126e7cdad2b56638269d34a85eed10d903122
ngeran <ngeran@gmail.com>
2025-03-23 09:22:15 -0400

    copilot

--- home-manager/features/cli/nixvim/plugins/copilot.nix
+++ home-manager/features/cli/nixvim/plugins/copilot.nix
@@ -2,11 +2,6 @@
   programs.nixvim = {
     plugins.copilot-lua = {
       enable = true;
-      settings = {
-        copilot = {
-          node_command = "/home/nikos/.nix-profile/bin/node";
-        };
-      };
     };
   };
 }
