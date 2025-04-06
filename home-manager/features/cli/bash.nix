c3567709387c04061d74d45121ae6e95c9ccca91
ngeran <ngeran@gmail.com>
2025-04-06 19:29:51 -0400

    starship

--- home-manager/features/cli/bash.nix
+++ home-manager/features/cli/bash.nix
@@ -2,9 +2,11 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    # Add a reference to your starship.toml file
+    # Shell initialization for Starship
     shellInit = ''
+      # Initialize Starship prompt for Bash
       export STARSHIP_CONFIG="$HOME/.config/starship.toml"
+      eval "$(starship init bash)"
     '';
   };
 }
