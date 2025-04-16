c6ed14c4b84b98051bb567a71e88e9794d997fa3
ngeran <ngeran@gmail.com>
2025-04-16 18:58:52 -0400

    yazi

--- home-manager/features/cli/yazi.nix
+++ home-manager/features/cli/yazi.nix
@@ -1,14 +1,16 @@
+{ config, pkgs, ... }:
+
 {
   programs.yazi = {
     enable = true;
     settings = {
       theme = {
-        name = "gruvbox-dark"; # The name (without .toml)
+        name = "gruvbox-dark"; # Assuming your file is named gruvbox-dark.toml
       };
     };
-    file."theme.toml" = { # Deploy the theme file to the correct location
-      source = ./themes/yazi/gruvbox-dark.toml; # Adjust the path if your file is elsewhere
-      target = ".config/yazi/theme.toml";
-    };
+  };
+
+  home.file.".config/yazi/theme.toml" = {
+    source = ./themes/yazi/gruvbox-dark.toml; # Adjust the path to your theme file
   };
 }
