a60fe6f14d6caa671875a36452f17fdcce002656
ngeran <ngeran@gmail.com>
2025-04-16 17:24:44 -0400

    snacks

--- home-manager/features/cli/nixvim/plugins/snacks.nix
+++ home-manager/features/cli/nixvim/plugins/snacks.nix
@@ -1,31 +1,120 @@
-{ lib, config, ... }:
 {
   programs.nixvim = {
     plugins.snacks = {
       enable = true;
-      settings.profiler.enabled = true;
-    };
+      type = "lua"; # Or "lazy" if you're using LazyVim
+      name = "user-snacks-config"; # A name for this plugin configuration
+      config = ''
+        local snacks = require("snacks") -- Assuming 'snacks' is the Lua module name
 
-    extraConfigLuaPre =
-      let
-        nixvim_cfg = config.programs.nixvim;
-      in
-      lib.mkOrder 1 (
-        lib.optionalString nixvim_cfg.plugins.snacks.settings.profiler.enabled # Lua
-          ''
-            -- if vim.env.PROF then
-            if vim.env.PROF then
-              local snacks = "${nixvim_cfg.plugins.snacks.package}"
-              vim.opt.rtp:append(snacks)
-              require("snacks.profiler").startup({
-                startup = {
-                  -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
-                  event = "UIEnter",
-                  -- event = "VeryLazy",
+        snacks.setup {
+          animate = {
+            enabled = true,
+          },
+          bigfile = {
+            enabled = true,
+          },
+          bufdelete = { },
+          dashboard = {
+            enabled = true,
+            formats = {
+              __raw = [[
+                {
+                  key = function(item)
+                    return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+                  end,
+                }
+              ]],
+            },
+            sections = {
+              __raw = [[
+                {
+                  { section = "header" },
+                  {
+                    pane = 1,
+                    section = "terminal",
+                    cmd = "colorscript -e square",
+                    padding = 1,
+                  },
+                  { section = "keys", gap = 1, padding = 1 },
+                  { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                  { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                  {
+                    pane = 2,
+                    height = 16,
+                    section = "terminal",
+                    cmd = "fortune -s | cowsay",
+                    padding = 1,
+                  },
                 },
-              })
-            end
-          ''
-      );
+              ]],
+            },
+          },
+          gitbrowse = {
+            enabled = true,
+          },
+          image = {
+            enabled = true,
+          },
+          indent = {
+            enabled = true,
+          },
+          input = {
+            enabled = true,
+          },
+          quickfile = {
+            enabled = true,
+          },
+          scope = {
+            enabled = true,
+          },
+          scroll = {
+            enabled = true,
+          },
+          notifier = {
+            enabled = true,
+            timeout = 3000,
+          },
+          statuscolumn = {
+            enabled = false,
+          },
+          picker = {
+            matcher = {
+              cwd_bonus = true,
+              frecency = true,
+              history_bonus = true,
+              sort_empty = true,
+            },
+            formatters = {
+              file = {
+                truncate = 200,
+              },
+            },
+            layout = {
+              __raw = [[
+                {
+                  layout = {
+                    box = "vertical",
+                    backdrop = false,
+                    row = -1,
+                    width = 0,
+                    height = 0,
+                    border = "top",
+                    title = " {title} {live} {flags}",
+                    title_pos = "left",
+                    { win = "input", height = 1, border = "bottom" },
+                    {
+                      box = "horizontal",
+                      { win = "list", border = "none", height = 0 },
+                      { win = "preview", title = "{preview}", width = 0.3, height = 0, border = "left" },
+                    },
+                  },
+                }
+              ]],
+            },
+          },
+        };
+      '';
+    };
   };
 }
