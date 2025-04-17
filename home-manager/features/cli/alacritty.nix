4f0467eeccc14a354e9f69fb5e5fab2a53e69543
ngeran <ngeran@gmail.com>
2025-04-16 20:19:37 -0400

    alacritty

--- home-manager/features/cli/alacritty.nix
+++ home-manager/features/cli/alacritty.nix
@@ -2,15 +2,7 @@
   programs.alacritty = {
     enable = true;
     settings = {
-     # window.opacity = 1.0; <-- Stylix contols the opacity 
-
-      font = {
-        builtin_box_drawing = true;
-        normal = {
-          style = lib.mkForce "Bold";
-        };
+     # window.opacity = 1.0; <-- Stylix contols the opacity
       };
-    };
   };
 }
-
