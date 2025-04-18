a09dffcabb193f11e541262ce6a981211774658d
ngeran <ngeran@gmail.com>
2025-04-18 08:38:59 -0400

    yazi-theme

--- home-manager/features/cli/yazi.nix
+++ home-manager/features/cli/yazi.nix
@@ -3,6 +3,23 @@
   programs.yazi = {
     enable = true;
     theme = {
+      manager = lib.mkForce {
+        cwd = { fg = "#83a598"; };
+        hovered = { fg = "#282828"; bg = "#83a598"; };
+        preview_hovered = { underline = true; };
+        find_keyword = { fg = "#b8bb26"; italic = true; };
+        find_position = { fg = "#fe8019"; bg = "reset"; italic = true; };
+        marker_selected = { fg = "#b8bb26"; bg = "#b8bb26"; };
+        marker_copied = { fg = "#b8bb26"; bg = "#b8bb26"; };
+        marker_cut = { fg = "#fb4934"; bg = "#fb4934"; };
+        tab_active = { fg = "#282828"; bg = "#504945"; };
+        tab_inactive = { fg = "#a89984"; bg = "#3c3836"; };
+        tab_width = 1;
+        border_symbol = "│";
+        border_style = { fg = "#665c54"; };
+        # syntect_theme = "~/.config/yazi/Gruvbox-Dark.tmTheme";
+      };
+
       status = lib.mkForce {
         separator_open = " "; # Using a common powerline separator
         separator_close = " ";
