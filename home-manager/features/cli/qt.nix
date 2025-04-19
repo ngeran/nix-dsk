d897fd60bb121339643ac01e3fa1c76a9a4d332f
ngeran <ngeran@gmail.com>
2025-04-19 08:07:29 -0400

    qt

--- home-manager/features/cli/qt.nix
+++ home-manager/features/cli/qt.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   home.packages = with pkgs; [
     papirus-icon-theme
     pcmanfm-qt
   ];
   qt = {
     enable = true;
-    platformTheme.name = "gtk";
+    platformTheme.name = lib.mkForce "gtk";
     style = {
       package = pkgs.adwaita-qt;
       name = "adwaita-dark";
