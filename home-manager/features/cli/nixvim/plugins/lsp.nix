adb8db06887bcda5fd1b73ab6f076257bb472716
ngeran <ngeran@gmail.com>
2025-04-27 10:22:39 -0400

    updates

--- home-manager/features/cli/nixvim/plugins/lsp.nix
+++ home-manager/features/cli/nixvim/plugins/lsp.nix
@@ -10,5 +10,30 @@
         rust_analyzer.enable = true;
       };
     };
+
+    # Enable the blink-cmp plugin and set it up for LSP completions
+    plugins.blink-cmp = {
+      enable = true;
+      setupLspCapabilities = true;
+
+      settings = {
+        keymap.preset = "super-tab";  # or another preset keybinding of your choice
+        signature.enabled = true;
+
+        sources = {
+          default = [
+            "buffer"
+            "lsp"  # Ensures LSP sources are included for completion
+            "path"
+            "snippets"
+            "copilot"
+            "dictionary"
+            "emoji"
+            "git"
+            "spell"
+          ];
+        };
+      };
+    };
   };
 }
