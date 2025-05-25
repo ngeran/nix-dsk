74113d773189a8b119ba0560e721b9016f464f1c
ngeran <ngeran@gmail.com>
2025-05-24 20:36:18 -0400

    update home-manager version

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
            };
            #===[HOME MANAGER]===
           home-manager = {
-             url = "github:nix-community/home-manager";
+             url = "github:nix-community/home-manager/release-25.05";
              inputs.nixpkgs.follows = "nixpkgs";
              };
 	};
