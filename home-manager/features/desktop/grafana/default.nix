432957504b2244de7bbefa77a54278940f682435
ngeran <ngeran@gmail.com>
2025-05-31 16:42:14 -0400

    grafana

--- home-manager/features/desktop/grafana/default.nix
+++ home-manager/features/desktop/grafana/default.nix
@@ -2,16 +2,8 @@
   # grafana configuration
   services.grafana = {
     enable = true;
-    domain = "webvector.com";
-    port = 2342;
+    domain = "vector.ngeran";
+    port = 3000;
     addr = "127.0.0.1";
-  };
-
-  # nginx reverse proxy
-  services.nginx.virtualHosts.${config.services.grafana.domain} = {
-    locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
-        proxyWebsockets = true;
-    };
   };
 }
