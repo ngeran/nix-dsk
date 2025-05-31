cf5bd4d2236d57c7402067243bd1afe056d1af50
ngeran <ngeran@gmail.com>
2025-05-31 16:29:29 -0400

    grafana

--- home-manager/features/desktop/grafana/default.nix
+++ home-manager/features/desktop/grafana/default.nix
@@ -1,14 +1,17 @@
 { config, pkgs, ... }: {
+  # grafana configuration
   services.grafana = {
     enable = true;
-    settings = {
-      server = {
-        http_addr = "127.0.0.1";
-        http_port = 3000;
-      };
-    };
+    domain = "webvector.com";
+    port = 2342;
+    addr = "127.0.0.1";
   };
 
-  # optional if you're exposing it remotely
-  networking.firewall.allowedTCPPorts = [ 3000 ];
+  # nginx reverse proxy
+  services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+    };
+  };
 }
