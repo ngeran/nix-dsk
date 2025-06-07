bd7ff1dd7a3422261d202c14f91bda14deb67fb7
ngeran <ngeran@gmail.com>
2025-06-07 14:28:36 -0400

    influx

--- hosts/common/global/influxdb/default.nix
+++ hosts/common/global/influxdb/default.nix
@@ -16,29 +16,22 @@
   services.telegraf = {
     enable = true;
 
-    # Use the structured options provided by the NixOS Telegraf module
-    agent = {
-      interval = "10s";
-      round_interval = true;
-      hostname = config.networking.hostName;
-      omitHostname = false; # NixOS option is usually camelCase
-    };
-
+    # Use a combined approach:
+    # 1. Use structured options for inputs and outputs (as these are well-defined NixOS options).
+    # 2. Use extraConfig for agent settings and any other sections that don't have direct NixOS options.
     inputs = {
-      # Use the specific input option for jti_openconfig_telemetry
       jti_openconfig_telemetry = [{
         servers = ["0.0.0.0:50051"];
-        sampleFrequency = "2000ms"; # NixOS option is usually camelCase
+        sampleFrequency = "2000ms";
         sensors = [
           "/network-instances/network-instance/protocols/protocol/bgp/neighbors"
           "/bgp-rib"
         ];
-        retryDelay = "1000ms"; # NixOS option is usually camelCase
+        retryDelay = "1000ms";
       }];
     };
 
     outputs = {
-      # Use the specific output option for influxdb_v2
       influxdb_v2 = [{
         urls = ["http://localhost:8086"];
         token = "abVGyimvFALQqA6H1bvWsvI1jyBs9HA5fmtge1KeMWYhcd0x_i35CeCBX-UMNFjBq8Vp3vZgdwCgTzCtt0-PKQ==";
@@ -47,10 +40,17 @@
       }];
     };
 
-    # Only use extraConfig for truly custom or unsupported Telegraf settings.
-    # In this case, we've covered everything with structured options,
-    # so we can probably remove extraConfig entirely or leave it empty.
-    # extraConfig = ""; # Or simply remove this line if nothing else is needed.
+    # Re-introduce extraConfig for the 'agent' section.
+    # Use lib.mkForce to ensure this string content takes precedence
+    # for the parts of the configuration that aren't covered by structured options.
+    extraConfig = lib.mkForce ''
+      # Global agent configuration
+      [agent]
+        interval = "10s"
+        round_interval = true
+        hostname = "${config.networking.hostName}"
+        omit_hostname = false
+    '';
   };
 
   networking.firewall.allowedTCPPorts = [
