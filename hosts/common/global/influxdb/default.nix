a27b7ce56fac18ed64acd963e8556f382b6850f2
ngeran <ngeran@gmail.com>
2025-06-07 14:08:34 -0400

    influx

--- hosts/common/global/influxdb/default.nix
+++ hosts/common/global/influxdb/default.nix
@@ -17,45 +17,6 @@
 
   services.telegraf = {
     enable = true;
-    extraConfig = ''
-      # Global agent configuration
-      [agent]
-        interval = "10s"
-        round_interval = true
-        hostname = "${config.networking.hostName}" # Identifies the Telegraf host
-        omit_hostname = false
-
-      # Juniper JTI OpenConfig Telemetry Input
-      # This is the Telegraf input plugin that listens for JTI data.
-      [[inputs.jti_openconfig_telemetry]]
-        ## List of device addresses (Telegraf listens on these) to collect telemetry from
-        # Telegraf will act as a gRPC server that Juniper devices connect to.
-        # Use 0.0.0.0 to listen on all interfaces.
-        # The port 50051 is from your cRPD configuration.
-        servers = ["0.0.0.0:50051"] # Telegraf listens for JTI on this IP/port
-        sample_frequency = "2000ms" # How often Telegraf tells Juniper to send data
-        sensors = [
-          "/network-instances/network-instance/protocols/protocol/bgp/neighbors",
-          "/bgp-rib",
-        ]
-        retry_delay = "1000ms"
-
-        # Based on your cRPD config `skip-authentication`, you might not need these.
-        # If authentication is required on cRPD, uncomment and set:
-        # username = "your_telemetry_user"
-        # password = "your_telemetry_password"
-
-      # InfluxDB 2.x Output
-      # Telegraf will send the collected data to your InfluxDB 2.x instance.
-      [[outputs.influxdb_v2]]
-        urls = ["http://localhost:8086"] # InfluxDB 2.x default HTTP address
-        token = "YOUR_GENERATED_INFLUXDB_TOKEN" # <-- REPLACE WITH THE TOKEN FROM STEP 2!
-        organization = "juniper_org" # <-- REPLACE WITH YOUR ORGANIZATION NAME FROM STEP 2!
-        bucket = "juniper_telemetry_bucket" # <-- REPLACE WITH YOUR BUCKET NAME FROM STEP 2!
-        # These settings ensure data is written correctly to InfluxDB 2.x.
-        # The database, username, password, and retention_policy from your cRPD doc
-        # are for InfluxDB 1.x and are mapped to bucket, organization, and token in InfluxDB 2.x.
-    '';
   };
 
   # Open the port for Telegraf to receive JTI data
