9e0ffebc54b8b2f39849f69b0b6bd55a88bbdc48
ngeran <ngeran@gmail.com>
2025-06-07 14:37:30 -0400

    influx

--- hosts/common/global/influxdb/default.nix
+++ hosts/common/global/influxdb/default.nix
@@ -1,6 +1,6 @@
 # /etc/nixos/configuration.nix
 
-{ config, pkgs, lib, ... }: # Ensure 'lib' is here
+{ config, pkgs, lib, ... }: # Ensure 'lib' is included
 
 {
   environment.systemPackages = with pkgs; [
@@ -16,34 +16,26 @@
   services.telegraf = {
     enable = true;
 
-    # The 'config' option is usually where you provide the structured configuration
-    # that the module then serializes into a TOML file for Telegraf.
-    # This is an attribute set.
-    config = {
-      # Global agent configuration
+    extraConfig = {
       agent = {
         interval = "10s";
         round_interval = true;
         hostname = config.networking.hostName;
-        omit_hostname = false; # Keep this as snake_case as it maps directly to Telegraf's TOML
+        omit_hostname = false;
       };
 
-      # Juniper JTI OpenConfig Telemetry Input
-      # This is a list of input plugin configurations.
       inputs = {
         jti_openconfig_telemetry = [{
           servers = ["0.0.0.0:50051"];
-          sample_frequency = "2000ms"; # Keep as snake_case for TOML
+          sample_frequency = "2000ms";
           sensors = [
             "/network-instances/network-instance/protocols/protocol/bgp/neighbors"
             "/bgp-rib"
           ];
-          retry_delay = "1000ms"; # Keep as snake_case for TOML
+          retry_delay = "1000ms";
         }];
       };
 
-      # InfluxDB 2.x Output
-      # This is a list of output plugin configurations.
       outputs = {
         influxdb_v2 = [{
           urls = ["http://localhost:8086"];
@@ -53,7 +45,6 @@
         }];
       };
     };
-    # No extraConfig here, as 'config' handles everything.
   };
 
   networking.firewall.allowedTCPPorts = [
@@ -61,5 +52,5 @@
     50051 # Juniper JTI gRPC (Telegraf listens here)
   ];
 
-  # ... rest of your configuration ...
+  # ... any other configuration ...
 }
