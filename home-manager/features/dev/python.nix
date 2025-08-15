815b87f71f215a1f39d53d19c21b729dd64c184d
ngeran <ngeran@gmail.com>
2025-08-15 14:47:21 -0400

    updates python.nix

--- home-manager/features/dev/python.nix
+++ home-manager/features/dev/python.nix
@@ -3,6 +3,34 @@
   config,
   ...
 }:
+let
+  jsnapy = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "jsnapy";
+    version = "1.3.8";
+
+    src = pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "b1a4c6f048af4b048ff843541da94384320876b80b0a8b97c99fb00b8614e57d";
+    };
+
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      junos-eznc
+      pyyaml
+      lxml
+      colorama  # Add this if not already present; it's a dependency
+    ];
+
+    # Skip tests as they may require additional setup (e.g., nose) or network access
+    doCheck = false;
+
+    meta = with pkgs.lib; {
+      description = "Junos Snapshot Administrator in Python";
+      homepage = "https://github.com/Juniper/jsnapy";
+      license = licenses.asl20;
+      maintainers = [];  # Optional
+    };
+  };
+in
 {
   home = {
     packages = with pkgs; [
@@ -30,15 +58,15 @@
         tabulate
         prometheus_client
         git-filter-repo
+        jsnapy  # Add this line
       ]))
-
     ];
 
     # Mypy cache directory
     sessionVariables.MYPY_CACHE_DIR = "${config.xdg.cacheHome}/mypy";
   };
 
-  # Program configurations
+  # Program configurations (unchanged)
   programs = {
     ruff = {
       enable = true;
