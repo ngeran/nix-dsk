c78958d725f10b6e55ac4d175effcaa89dbdb4bc
ngeran <ngeran@gmail.com>
2025-08-15 15:05:30 -0400

    revert-to-the-original

--- home-manager/features/dev/python.nix
+++ home-manager/features/dev/python.nix
@@ -3,54 +3,10 @@
   config,
   ...
 }:
-let
-  jsnapy = pkgs.python3Packages.buildPythonPackage rec {
-    pname = "jsnapy";
-    version = "1.3.8";
-    pyproject = true;
-
-    src = pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "b1a4c6f048af4b048ff843541da94384320876b80b0a8b97c99fb00b8614e57d";
-    };
-
-    build-system = with pkgs.python3Packages; [ setuptools ];
-
-    propagatedBuildInputs = with pkgs.python3Packages; [
-      junos-eznc
-      pyyaml
-      lxml
-      colorama
-    ];
-
-    # Skip tests as they may require additional setup or network access
-    doCheck = false;
-
-    # Patch to prevent installation to /etc/jsnapy
-    postPatch = ''
-      substituteInPlace setup.py \
-        --replace "'data_files': [('/etc/jsnapy', ['jnpr/jsnapy/jsnapy.cfg', 'jnpr/jsnapy/logging.yml'])]," "" \
-        --replace "'/etc/jsnapy'," ""
-    '';
-
-    # Ensure configuration files are installed in the Nix store
-    postInstall = ''
-      mkdir -p $out/etc/jsnapy
-      cp jnpr/jsnapy/jsnapy.cfg $out/etc/jsnapy/
-      cp jnpr/jsnapy/logging.yml $out/etc/jsnapy/
-    '';
-
-    meta = with pkgs.lib; {
-      description = "Junos Snapshot Administrator in Python";
-      homepage = "https://github.com/Juniper/jsnapy";
-      license = licenses.asl20;
-      maintainers = [];
-    };
-  };
-in
 {
   home = {
     packages = with pkgs; [
+      # Python packages
       (python3.withPackages (pypkgs: with pypkgs; [
         pip
         requests
@@ -74,23 +30,26 @@ in
         tabulate
         prometheus_client
         git-filter-repo
-        jsnapy
       ]))
+
     ];
 
+    # Mypy cache directory
     sessionVariables.MYPY_CACHE_DIR = "${config.xdg.cacheHome}/mypy";
   };
 
+  # Program configurations
   programs = {
     ruff = {
       enable = true;
+
       settings = {
         line-length = 100;
       };
     };
 
     nixvim = {
-      filetype.extension.gin = "gin";
+      filetype.extension.gin = "gin"; # inria
       files."after/ftplugin/python.lua" = {
         autoCmd = [
           {
@@ -107,7 +66,7 @@ in
         ];
       };
       plugins = {
-        treesitter.languageRegister.python = [ "gin" ];
+        treesitter.languageRegister.python = [ "gin" ]; # inria
         lsp.servers = {
           ruff.enable = true;
           pylsp = {
