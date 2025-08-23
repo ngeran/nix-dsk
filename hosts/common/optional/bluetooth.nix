6a28a9319aae47eab37ded01bdd2d96b4d0e8453
ngeran <ngeran@gmail.com>
2025-08-23 11:34:00 -0400

    bluetooth

--- hosts/common/optional/bluetooth.nix
+++ hosts/common/optional/bluetooth.nix
@@ -1,8 +1,23 @@
 {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
-  # hardware.enableAllFirmware = true;
-  hardware.enableRedistributableFirmware = true;
+  # Enable Bluetooth hardware
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
 
+  # Enable the BlueZ service (required for PipeWire to see Bluetooth devices)
+  services.blueman.enable = true;
+  services.bluez = {
+    enable = true;
+    # Optional: better codec support (PipeWire handles this too)
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+      };
+    };
+  };
+
+  # Ensure firmware availability
+  hardware.enableRedistributableFirmware = true;
 }
