475ba46f46c8e9afb02cc43cb0511941d3efc043
ngeran <ngeran@gmail.com>
2025-08-23 16:29:12 -0400

    lmstudio

--- home-manager/features/ai/lmstudio.nix
+++ home-manager/features/ai/lmstudio.nix
@@ -1,24 +1,9 @@
 { config, pkgs, ... }:
 
-let
-  lmstudioWrapped = pkgs.stdenv.mkDerivation rec {
-    pname = "lmstudio-wrapper";
-    version = "0.1";
-
-    src = null; # no source to unpack
-
-    buildInputs = [ pkgs.makeWrapper pkgs.lmstudio pkgs.qt5.qtbase pkgs.libGL ];
-
-    installPhase = ''
-      mkdir -p $out/bin
-      makeWrapper ${pkgs.lmstudio}/bin/lmstudio $out/bin/lmstudio \
-        --set LD_LIBRARY_PATH ${pkgs.qt5.qtbase}/lib:${pkgs.libGL}/lib \
-        --set QT_QPA_PLATFORM xcb \
-        --set OZONE_PLATFORM_HINT auto
-    '';
-  };
-in {
-  home.packages = [ lmstudioWrapped ];
+{
+  home.packages = with pkgs; [
+    lmstudio
+  ];
 
   xdg.desktopEntries.lm-studio = {
     name = "LM Studio";
