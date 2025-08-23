dc71c19a629f1f9d9ecb2549905c8d718b87c5f0
ngeran <ngeran@gmail.com>
2025-08-23 14:20:32 -0400

    bluetooth

--- hosts/common/optional/bluetooth.nix
+++ hosts/common/optional/bluetooth.nix
@@ -1,8 +1,11 @@
+{ config, lib, pkgs, ... }:
+
 {
   # Enable Bluetooth hardware + bluetoothd service
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    package = pkgs.bluez; # Explicitly use BlueZ
   };
 
   # Optional GUI manager for Bluetooth
@@ -10,4 +13,7 @@
 
   # Ensure firmware blobs are available
   hardware.enableRedistributableFirmware = true;
+
+  # Force-load MediaTek MT7921e driver at boot
+  boot.kernelModules = [ "mt7922" ];
 }
